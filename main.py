3457714222afbb7322387f41881af4e0de307d27
윤성필 <starpil.python@gmail.com>
2025-06-06 15:34:03 +0000

    1차수정(20250607)

--- main.py
+++ main.py
@@ -2,13 +2,13 @@
 
 from fastapi import FastAPI
 import uvicorn # 배포용 서버 
-from routers import router as item_router  # 라우터 가져오기
+from routers import router as all_router  # 라우터 가져오기
 from starlette.middleware.sessions import SessionMiddleware
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.add_middleware(SessionMiddleware, secret_key="your_secret_key") # 새션 추가 
-app.include_router(item_router)  # 라우터 포함시키기
+app.include_router(all_router)  # 라우터 포함시키기
 
 app.add_middleware(
     CORSMiddleware,
@@ -21,3 +21,4 @@ app.add_middleware(
 if __name__ == '__main__':
     # 배포용으로 설정 
     uvicorn.run(app,host='0.0.0.0',port=1025, log_level="info")
+
--- routers/__init__.py
+++ routers/__init__.py
@@ -5,6 +5,6 @@ from .c3_char_select.router import router as c3_char_selecter
 # 메인 라우터 호출 
 router = APIRouter()
 
-router.include_router(c3_char_selecter, prefix="/users", tags=["users"])
+router.include_router(c3_char_selecter, prefix="/child", tags=["child"])
 
 
